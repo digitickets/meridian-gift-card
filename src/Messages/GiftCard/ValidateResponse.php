155debbad3799ff6b7539068181e52232f025c75
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2020-04-18 14:45:51 +0100

    Make the validate response determine the current balance and make it available.

--- src/Messages/GiftCard/ValidateResponse.php
+++ src/Messages/GiftCard/ValidateResponse.php
@@ -3,7 +3,32 @@
 namespace DigiTickets\MeridianGiftCard\Messages\GiftCard;
 
 use DigiTickets\MeridianGiftCard\Messages\Common\AbstractVoucherResponse;
+use Omnipay\Common\Message\RequestInterface;
 
 class ValidateResponse extends AbstractVoucherResponse
 {
+    /**
+     * @var float|null
+     */
+    private $balance;
+
+    public function __construct(RequestInterface $request, $response)
+    {
+        parent::__construct($request, $response);
+
+        // We need to extract the current balance out of the response. The parent constructor will have determined
+        // whether the reponse was valid or not.
+        $this->balance = null;
+        if ($this->isSuccessful()) {
+            if (property_exists($response->Meridian, 'BalanceEnquiry') &&
+                property_exists($response->Meridian->BalanceEnquiry, 'Balance')) {
+                $this->balance = (float) $response->Meridian->BalanceEnquiry->Balance;
+            }
+        }
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
 }
